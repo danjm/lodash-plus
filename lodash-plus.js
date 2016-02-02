241f9e7c2042fa2e47627de13aa82dac4eaa884d
Dan Miller <danmiller@bluedrop.com>
2016-02-02 02:03:25 -0330

    First draft: isTruthy, isFalsy, pickTruthy

---
+++ lodash-plus.js
@@ -0,0 +1,30 @@
+var _ = require('lodash');
+
+_.mixin({
+	isFalsy: function (val) {
+		return !Boolean(val);
+	},
+	isTruthy: function (val) {
+		return Boolean(val);
+	},
+	pickTruthy: (obj, props) {
+		if (!_.isObject(obj)) {
+			throw new Error('Invalid params');
+		}
+		if (arguments.length === 1) {
+			return _.pickBy(obj, this.isTruthy);
+		}
+		else if (_.isString(props)) {
+			// What should the false case return here?
+			return this.isTruthy(obj[props]) ? _.pick(obj, props) : {};
+		}
+		else if (_.isArray(props)) {
+			return _.pickBy(_.pick(obj, props), this.isTruthy);
+		}
+		else {
+			throw new Error('Invalid params');
+		}
+	}
+});
+
+// TODO: specs
