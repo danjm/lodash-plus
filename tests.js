40b37492454f885011689464a27af014c33cb28c
Dan Miller <danmiller@bluedrop.com>
2016-02-05 22:56:18 -0330

    pickTruthy tests complete

--- tests.js
+++ tests.js
@@ -15,6 +15,10 @@ var falsyAndTruthy = {
 	k: true
 };
 
+var falsyParams = ['a', 'b', 'c', 'd', 'e', 'f'];
+var truthyParams = ['g', 'h', 'i', 'j', 'k'];
+var nonExistantParams = ['l', 'm', 'n'];
+
 describe('lodash-plus', function () {
 	describe('isFalsy', function() {
 		it('should return the same values as !Boolean()', function () {
@@ -33,13 +37,70 @@ describe('lodash-plus', function () {
 	});
 	
 	describe('pickTruthy', function() {
-		it('should pick all truthy values if one param passed in', function () {
-			assert.deepEqual(
-				_.pickTruthy(falsyAndTruthy),
-				_.pick(falsyAndTruthy, ['g', 'h', 'i', 'j', 'k'])
-			);
+		describe('when one param passed in', function () {
+			it('should pick all truthy values if one param passed in', function () {
+				assert.deepEqual(
+					_.pickTruthy(falsyAndTruthy),
+					_.pick(falsyAndTruthy, truthyParams)
+				);
+			});
 		});
 		
-		// TODO: specs needed for all pickTruthy cases
+		describe('when two params passed in and second is a string', function () {
+			it('should return an empty object if no property with the string name exists', function () {
+				assert.deepEqual(
+					_.pickTruthy(falsyAndTruthy, ''),
+					{}
+				);
+			});
+			
+			it('should return an empty object if the property with the string name is falsy', function () {
+				_.each(falsyParams, function (prop) {
+					assert.deepEqual(
+						_.pickTruthy(falsyAndTruthy, prop),
+						{}
+					);
+				});
+			});
+			
+			it('should return an object containing only the property if the property with the string name is truthy', function () {
+				_.each(truthyParams, function (prop) {
+					assert.deepEqual(
+						_.pickTruthy(falsyAndTruthy, prop),
+						_.set({}, prop, falsyAndTruthy[prop])
+					);
+				});
+			});
+		});
+		
+		describe('when two params passed in and second is an array', function () {
+			it('should return an empty object if no properties in the array exist', function () {
+				assert.deepEqual(
+					_.pickTruthy(falsyAndTruthy, nonExistantParams),
+					{}
+				);
+			});
+			
+			it('should return an empty object if the properties are falsy', function () {
+				assert.deepEqual(
+					_.pickTruthy(falsyAndTruthy, falsyParams.slice(0, 3)),
+					{}
+				);
+			});
+			
+			it('should return an object containing only the properties if they are truthy', function () {
+				assert.deepEqual(
+					_.pickTruthy(falsyAndTruthy, truthyParams.slice(0, 3)),
+					_.pick(falsyAndTruthy, truthyParams.slice(0, 3))
+				);
+			});
+			
+			it('should return an object containing only the truthy params', function () {
+				assert.deepEqual(
+					_.pickTruthy(falsyAndTruthy, nonExistantParams.concat(truthyParams.slice(1, 4)).concat(falsyParams.slice(2, 4))),
+					_.pick(falsyAndTruthy, truthyParams.slice(1, 4))
+				);
+			});
+		});
 	});
 });
