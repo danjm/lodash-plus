b9795389b6f91bd4b49b96e66f06cdd200be2d6c
Dan Miller <danmiller@bluedrop.com>
2016-04-03 13:05:17 -0230

    Adds tests task to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
+var mocha = require('gulp-mocha');
 
 gulp.task('lint', function () {
     return gulp.src(['**/*.js','!node_modules/**'])
@@ -8,10 +9,15 @@ gulp.task('lint', function () {
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('watch', function() {
-    gulp.watch('**/*.js', ['lint']);
+gulp.task('tests', function () {
+    return gulp.src('tests.js', {read: false})
+        .pipe(mocha());
 });
 
-gulp.task('default', ['lint', 'watch'], function () {
+gulp.task('watch', function() {
+    gulp.watch('**/*.js', ['lint', 'tests']);
+});
+
+gulp.task('default', ['lint', 'tests', 'watch'], function () {
     console.log('Success!');
 });
